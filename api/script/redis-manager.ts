98a8f71d08de86b83b530bdb0a395d8535d50bad
4J <roy8119425@gmail.com>
2025-05-05 13:01:29 +0800

    Disable auth & tls

--- api/script/redis-manager.ts
+++ api/script/redis-manager.ts
@@ -102,11 +102,11 @@ export class RedisManager {
       const redisConfig = {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        auth_pass: process.env.REDIS_KEY,
-        tls: {
-          // Note: Node defaults CA's to those trusted by Mozilla
-          rejectUnauthorized: true,
-        },
+        // auth_pass: process.env.REDIS_KEY,
+        // tls: {
+        //   // Note: Node defaults CA's to those trusted by Mozilla
+        //   rejectUnauthorized: true,
+        // },
       };
       this._opsClient = redis.createClient(redisConfig);
       this._metricsClient = redis.createClient(redisConfig);
